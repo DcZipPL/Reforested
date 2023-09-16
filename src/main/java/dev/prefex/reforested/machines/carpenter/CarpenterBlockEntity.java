ee3473a1cdbd8bae423197641e2a2966acf5bf9b
Kordian Kisieliński <dczip89@gmail.com>
2023-09-16 02:04:25 +0200

    Implement crafting result for real

--- src/main/java/dev/prefex/reforested/machines/carpenter/CarpenterBlockEntity.java
+++ src/main/java/dev/prefex/reforested/machines/carpenter/CarpenterBlockEntity.java
@@ -14,9 +14,11 @@ import net.minecraft.item.Items;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.RecipeManager;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -70,12 +72,60 @@ public class CarpenterBlockEntity extends MachineBlockEntity {
 			} else {
 				self.processTime = 0;
 				//self.maxProcessTime = 0;
-				self.inventory.set(11, recipe.getResult(world.getRegistryManager())[0].copy());
+				craftRecipe(world.getRegistryManager(), recipe, self.inventory, self.getMaxCountPerStack());
 			}
 		} else {
 			self.inventory.set(10, ItemStack.EMPTY);
 			self.processTime = 0;
 			//self.maxProcessTime = 0;
+		}
+	}
+
+	private static boolean isRecipeEmpty(DefaultedList<ItemStack> slots) {
+		return slots.stream().limit(9).allMatch(ItemStack::isEmpty); // 10th slot is optional
+	}
+
+	private static DefaultedList<ItemStack> getCraftingSlots(DefaultedList<ItemStack> slots) {
+		return slots.stream().limit(10).collect(DefaultedListCollector.toList());
+	}
+
+	private static boolean craftRecipe(DynamicRegistryManager registryManager, @Nullable CarpenterRecipe recipe, DefaultedList<ItemStack> slots, int maxCount) {
+		if (recipe != null && canAcceptRecipeOutput(registryManager, recipe, slots, maxCount)) {
+			DefaultedList<ItemStack> ingredients = getCraftingSlots(slots);
+			ItemStack result = recipe.getResult(registryManager)[0];
+			ItemStack resultSlot = slots.get(11);
+			if (resultSlot.isEmpty()) {
+				slots.set(11, result.copy());
+			} else if (resultSlot.isOf(result.getItem())) {
+				resultSlot.increment(result.getCount());
+			}
+
+			ingredients.forEach(stack -> stack.decrement(1));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static boolean canAcceptRecipeOutput(DynamicRegistryManager registryManager, @Nullable CarpenterRecipe recipe, DefaultedList<ItemStack> slots, int maxCount) {
+		if (!isRecipeEmpty(slots) && recipe != null) {
+			ItemStack recipeResult = recipe.getResult(registryManager)[0];
+			if (recipeResult.isEmpty()) {
+				return false;
+			} else {
+				ItemStack resultSlot = slots.get(11);
+				if (resultSlot.isEmpty()) {
+					return true;
+				} else if (!ItemStack.areItemsEqual(resultSlot, recipeResult)) {
+					return false;
+				} else if (resultSlot.getCount() < maxCount && resultSlot.getCount() < resultSlot.getMaxCount()) {
+					return true;
+				} else {
+					return resultSlot.getCount() < recipeResult.getMaxCount();
+				}
+			}
+		} else {
+			return false;
 		}
 	}
 
